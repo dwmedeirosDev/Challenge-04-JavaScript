302c8ea34bff61d76d40ff8dfd4111d79c760f3f
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-06-16 00:02:27 -0300

    Done

---
+++ README.txt
@@ -0,0 +1,12 @@
+//1 - Crie uma função que exiba uma mensagem no console
+
+//2 - Crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+//3 - Crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+
+//4 - Crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+
+### Bônus ###
+
+//5 - Crie uma função que retorne o triplo do número recebido no parâmetro da função
---
+++ script.js
@@ -0,0 +1,44 @@
+// ##### Exercício 1 #####
+
+// function message(){
+//     console.log("Exercício 1 OK");
+// }
+// message();
+
+
+
+// ##### Exercício 2 #####
+
+// function myName(name){
+//     console.log("Seu nome é:", name);
+// }
+
+// myName("David");
+
+
+
+// ##### Exercício 3 #####
+
+// function data(name,age,style){
+//     console.log(name,age,style);
+// }
+// data("David", "28 anos", "Rock & Eletrônica");
+
+
+
+// ##### Exercício 4 #####
+
+// function data(movie, music){
+//     console.log(movie, music);
+// }
+// data("Posto de combate", "Carry on Wayward Son");
+
+
+
+// ##### Exercício 5 #####
+
+// function value(number1){
+//     return number1 *3
+// }
+// console.log("O valor da multiplicação por 3 é", value(10));
+
